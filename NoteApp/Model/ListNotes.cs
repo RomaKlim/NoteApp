48cb939f1fac93457d56fae7c852b2814b43b4b5
Роман Климентьев <Roma-1998-rus@yandex.ru>
2019-01-17 21:16:46 +0700

    Реализованны классы бизнес логики

--- NoteApp/Model/ListNotes.cs
+++ NoteApp/Model/ListNotes.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Model
+namespace NoteApp.Model
 {
-    class ListNotes
+    /// <summary>
+    /// Перечень всех записок.
+    /// </summary>
+    public class ListNotes
     {
+        public List<Note> Notes;
+
+        public Note LastSelectedNote = null;
+
+        public ListNotes()
+        {
+            Notes = new List<Note>();
+        }
+
+        public void Sort()
+        {
+            Notes.Sort((a, b) => b.LastChangeTime.CompareTo(a.LastChangeTime));
+        }
+
+        public List<Note> Sort(int category)
+        {
+            Sort();
+            if (category >= 0)
+            {
+                return Notes.FindAll(a => a.Category == (NoteCategory)category);
+            }
+            return Notes;
+        }
     }
+
+
 }
--- NoteApp/Model/ProjectManager.cs
+++ NoteApp/Model/ProjectManager.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
-namespace Model
+namespace NoteApp.Model
 {
-    class ProjectManager
+    public class ProjectManager
     {
+        // Путь в папку "Документы".
+        public static string PathToDocuments = Environment.GetFolderPath(Environment.SpecialFolder.Personal) + @"\NoteApp.notes";
+
+        public static void SaveToFile(ListNotes listNotes, string fileTitle)
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            //Открытие потока
+            using (StreamWriter sw = new StreamWriter(fileTitle))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                //Вызываем сериализацию и передаём объек для сериализации
+                serializer.Serialize(writer, (ListNotes)listNotes);
+            }
+        }
+
+        public static ListNotes Load(string fileTitle)
+        {
+            ListNotes notes = new ListNotes();
+            //Создаём экземпляр сериализатора.
+            JsonSerializer serializer = new JsonSerializer();
+            //Открываем поток для чтения из файла с указанием пути.
+            using (StreamReader sr = new StreamReader(fileTitle))
+            using (JsonReader reader = new JsonTextReader(sr))
+            {
+                //Вызываем десериализацию 
+                var noteList = (ListNotes)serializer.Deserialize<ListNotes>(reader);
+                notes = noteList;
+            }
+
+            return notes;
+        }
     }
 }
